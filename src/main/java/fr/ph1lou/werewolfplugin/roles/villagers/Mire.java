2e6018dc81149c4a9e31b1285e8289d549f2ce6b
Baruff54 <43937533+Baruff54@users.noreply.github.com>
2022-07-10 13:59:36 +0200

    Commit mire

--- src/main/java/fr/ph1lou/werewolfplugin/roles/villagers/Mire.java
+++ src/main/java/fr/ph1lou/werewolfplugin/roles/villagers/Mire.java
@@ -1,8 +1,12 @@
 package fr.ph1lou.werewolfplugin.roles.villagers;
 
-import fr.ph1lou.werewolfapi.enums.Camp;
-import fr.ph1lou.werewolfapi.enums.Prefix;
-import fr.ph1lou.werewolfapi.enums.StatePlayer;
+import fr.ph1lou.werewolfapi.annotations.IntValue;
+import fr.ph1lou.werewolfapi.annotations.Role;
+import fr.ph1lou.werewolfapi.basekeys.ConfigBase;
+import fr.ph1lou.werewolfapi.basekeys.IntValueBase;
+import fr.ph1lou.werewolfapi.basekeys.Prefix;
+import fr.ph1lou.werewolfapi.basekeys.RoleBase;
+import fr.ph1lou.werewolfapi.enums.*;
 import fr.ph1lou.werewolfapi.game.WereWolfAPI;
 import fr.ph1lou.werewolfapi.player.impl.PotionModifier;
 import fr.ph1lou.werewolfapi.player.interfaces.IPlayerWW;
@@ -24,40 +28,52 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Role(key = RoleBase.MIRE,
+        category = Category.VILLAGER,
+        attributes = {RoleAttribute.VILLAGER, RoleAttribute.MINOR_INFORMATION},
+        configValues = @IntValue(
+                key = IntValueBase.MIRE_DISTANCE,
+                defaultValue = 60,
+                meetUpValue = 60,
+                step = 5,
+                item = UniversalMaterial.SKELETON_SKULL
+        )
+)
 public class Mire extends RoleVillage {
     private IPlayerWW lowLife;
     private double life;
 
-    public Mire(WereWolfAPI game, IPlayerWW playerWW, String key) {
-        super(game, playerWW, key);
+    public Mire(WereWolfAPI game, IPlayerWW playerWW) {
+        super(game, playerWW);
     }
 
     @Override
     public @NotNull String getDescription() {
         return new DescriptionBuilder(game, this)
-                .setDescription(game.translate("werewolf.role.mire.desc"))
+                .setDescription(game.translate("werewolf.roles.mire.desc"))
                 .build();
     }
 
     @EventHandler
     public void playerDamage(EntityDamageEvent event) {
         if(!(event.getEntity() instanceof Player)) return;
-        Bukkit.broadcastMessage("C'est bien un joueur!");
 
         Player player = (Player) event.getEntity();
         IPlayerWW playerWW = game.getPlayerWW(player.getUniqueId()).orElse(null);
 
-        assert playerWW != null;
-        if(playerWW.getHealth() > 6) return;
-        Bukkit.broadcastMessage("Il est en dessous de 3 coeurs.");
+        if(playerWW == null) return;
 
-        if(playerWW == getPlayerWW()) {
+        if(!playerWW.isState(StatePlayer.ALIVE)) return;
+
+        if(playerWW.getHealth() > 6) return;
+
+        if(playerWW.equals(getPlayerWW())) {
             playerWW.addPotionModifier(
+                    //TODO : FAIRE CONFIG POUR AJOUTER QU'UN COEUR
                     PotionModifier.add(PotionEffectType.ABSORPTION, 600, 0, "mire"));
-            Bukkit.broadcastMessage("C'est le joueur qui détiens le role.");
         } else {
-            if((getPlayerWW().getLocation().distance(playerWW.getLocation()) < 60)) {
-                getPlayerWW().sendMessageWithKey(Prefix.ORANGE.getKey(), "werewolf.role.mire.warning_death");
+            if((getPlayerWW().getLocation().distance(playerWW.getLocation()) < game.getConfig().getValue(IntValueBase.MIRE_DISTANCE))) {
+                getPlayerWW().sendMessageWithKey(Prefix.ORANGE, "werewolf.roles.mire.warning_death");
             }
         }
     }
@@ -65,6 +81,8 @@ public class Mire extends RoleVillage {
     @EventHandler
     public void eatGoldenApple(PlayerItemConsumeEvent event) {
         if (!event.getItem().getType().equals(Material.GOLDEN_APPLE)) return;
+
+        if(!event.getPlayer().getUniqueId().equals(getPlayerUUID())) return;
 
         List<IPlayerWW> playerWWS = Bukkit.getOnlinePlayers()
                 .stream()
@@ -74,10 +92,10 @@ public class Mire extends RoleVillage {
                 .map(Optional::get)
                 .filter(playerWW -> playerWW.isState(StatePlayer.ALIVE))
                 .map(IPlayerWW::getRole)
-                .filter(roles -> roles.isDisplayCamp(Camp.WEREWOLF.getKey()))
                 .map(IRole::getPlayerWW)
                 .collect(Collectors.toList());
 
+        lowLife = null;
         life = 100;
         playerWWS.forEach(iPlayerWW -> {
             double vie = iPlayerWW.getHealth() / iPlayerWW.getMaxHealth() * 100;
@@ -87,7 +105,10 @@ public class Mire extends RoleVillage {
             lowLife = iPlayerWW;
         });
 
-        getPlayerWW().sendMessageWithKey(Prefix.ORANGE.getKey(), "werewolf.role.mire.min_health", Formatter.player(lowLife.getName()));
+        if(lowLife == null)
+            getPlayerWW().sendMessageWithKey(Prefix.ORANGE, "werewolf.roles.mire.all_full");
+        else
+            getPlayerWW().sendMessageWithKey(Prefix.ORANGE, "werewolf.roles.mire.min_health", Formatter.player(lowLife.getName()));
     }
 
     @Override
