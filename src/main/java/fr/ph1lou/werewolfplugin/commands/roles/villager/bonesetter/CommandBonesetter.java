dd8c312e1fc3e75fd4653d09c6d143c7c78d8e1c
Hephaisto-dev <hephaistopl@gmail.com>
2023-01-11 01:05:18 +0100

    Finished Bonesetter

---
+++ src/main/java/fr/ph1lou/werewolfplugin/commands/roles/villager/bonesetter/CommandBonesetter.java
@@ -0,0 +1,55 @@
+package fr.ph1lou.werewolfplugin.commands.roles.villager.bonesetter;
+
+import fr.ph1lou.werewolfapi.annotations.RoleCommand;
+import fr.ph1lou.werewolfapi.basekeys.Prefix;
+import fr.ph1lou.werewolfapi.basekeys.RoleBase;
+import fr.ph1lou.werewolfapi.commands.ICommandRole;
+import fr.ph1lou.werewolfapi.enums.StatePlayer;
+import fr.ph1lou.werewolfapi.game.WereWolfAPI;
+import fr.ph1lou.werewolfapi.player.interfaces.IPlayerWW;
+import fr.ph1lou.werewolfapi.player.utils.Formatter;
+import fr.ph1lou.werewolfapi.role.interfaces.IRole;
+import fr.ph1lou.werewolfplugin.roles.villagers.Bonesetter;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+@RoleCommand(key = "werewolf.roles.bonesetter.command",
+        roleKeys = RoleBase.BONESETTER,
+        argNumbers = 1)
+public class CommandBonesetter implements ICommandRole {
+    @Override
+    public void execute(WereWolfAPI game, IPlayerWW playerWW, String[] args) {
+        IRole role = playerWW.getRole();
+        if (!(role instanceof Bonesetter)) {
+            return;
+        }
+        Bonesetter bonesetter = (Bonesetter) role;
+
+        if(bonesetter.getUse() == 3) {
+            playerWW.sendMessageWithKey(Prefix.RED, "werewolf.roles.bonesetter.too_many_players");
+            return;
+        }
+
+        Player target = Bukkit.getPlayer(args[0]);
+        if(target == null) {
+            playerWW.sendMessageWithKey(Prefix.RED , "werewolf.check.offline_player");
+            return;
+        }
+
+        IPlayerWW targetWW = game.getPlayerWW(target.getUniqueId()).orElse(null);
+
+        if(targetWW == null || !targetWW.isState(StatePlayer.ALIVE)) {
+            playerWW.sendMessageWithKey(Prefix.RED , "werewolf.check.player_not_found");
+            return;
+        }
+
+        if(playerWW.getUUID().equals(targetWW.getUUID())) {
+            playerWW.sendMessageWithKey(Prefix.RED , "werewolf.check.not_yourself");
+            return;
+        }
+
+        bonesetter.setUse(bonesetter.getUse() + 1);
+        bonesetter.addAffectedPlayer(targetWW);
+        playerWW.sendMessageWithKey(Prefix.GREEN, "werewolf.roles.bonesetter.perform", Formatter.player(targetWW.getName()));
+    }
+}
---
+++ src/main/java/fr/ph1lou/werewolfplugin/roles/villagers/Bonesetter.java
@@ -0,0 +1,123 @@
+package fr.ph1lou.werewolfplugin.roles.villagers;
+
+import fr.ph1lou.werewolfapi.annotations.Role;
+import fr.ph1lou.werewolfapi.basekeys.Prefix;
+import fr.ph1lou.werewolfapi.basekeys.RoleBase;
+import fr.ph1lou.werewolfapi.enums.Aura;
+import fr.ph1lou.werewolfapi.enums.Category;
+import fr.ph1lou.werewolfapi.enums.RoleAttribute;
+import fr.ph1lou.werewolfapi.events.roles.bonesetter.BonesetterHealEvent;
+import fr.ph1lou.werewolfapi.game.WereWolfAPI;
+import fr.ph1lou.werewolfapi.player.impl.PotionModifier;
+import fr.ph1lou.werewolfapi.player.interfaces.IPlayerWW;
+import fr.ph1lou.werewolfapi.role.impl.RoleVillage;
+import fr.ph1lou.werewolfapi.role.interfaces.IAffectedPlayers;
+import fr.ph1lou.werewolfapi.role.interfaces.ILimitedUse;
+import fr.ph1lou.werewolfapi.role.utils.DescriptionBuilder;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.potion.PotionEffectType;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Role(key = RoleBase.BONESETTER,
+        category = Category.VILLAGER,
+        attributes = RoleAttribute.VILLAGER)
+public class Bonesetter extends RoleVillage implements IAffectedPlayers, ILimitedUse {
+    private final List<IPlayerWW> affectedPlayers = new ArrayList<>();
+    private int use = 0;
+
+    public Bonesetter(WereWolfAPI game, IPlayerWW playerWW) {
+        super(game, playerWW);
+    }
+
+    @Override
+    public @NotNull String getDescription() {
+        return new DescriptionBuilder(this.game, this)
+                .setDescription(this.game.translate("werewolf.roles.bonesetter.description"))
+                .build();
+    }
+
+    @Override
+    public void recoverPower() {
+
+    }
+
+    @EventHandler
+    public void onDamage(EntityDamageEvent event){
+        if (!isAbilityEnabled()) return;
+
+        if (!(event.getEntity() instanceof Player)) return;
+
+        if (((Player) event.getEntity()).getHealth() - event.getFinalDamage() < 2 * 4) return;
+
+        IPlayerWW targetWW = game.getPlayersWW().stream()
+                .filter(playerWW -> !playerWW.equals(getPlayerWW()))
+                .filter(affectedPlayers::contains)
+                .findFirst()
+                .orElse(null);
+
+        if (targetWW == null) return;
+
+        BonesetterHealEvent bonesetterHealEvent = new BonesetterHealEvent(getPlayerWW(), targetWW);
+        Bukkit.getPluginManager().callEvent(bonesetterHealEvent);
+        if(!bonesetterHealEvent.isCancelled()) {
+            targetWW.addPotionModifier(PotionModifier.add(PotionEffectType.REGENERATION, 20 * 7, 1, getKey()));
+            targetWW.sendMessageWithKey(Prefix.GREEN, "werewolf.roles.bonesetter.receive_heal");
+            getPlayerWW().sendMessageWithKey(Prefix.YELLOW, "werewolf.roles.bonesetter.activate");
+            affectedPlayers.remove(targetWW);
+        }
+    }
+
+    @EventHandler
+    public void onConsume(PlayerItemConsumeEvent event){
+        if (!event.getPlayer().getUniqueId().equals(getPlayerUUID())) return;
+
+        if (event.getItem().getType().equals(Material.GOLDEN_APPLE)){
+            getPlayerWW().addPotionModifier(PotionModifier.add(PotionEffectType.REGENERATION, 20 * 7, 1, getKey()));
+        }
+    }
+
+    @Override
+    public Aura getDefaultAura() {
+        return Aura.NEUTRAL;
+    }
+
+    @Override
+    public void addAffectedPlayer(IPlayerWW iPlayerWW) {
+        this.affectedPlayers.add(iPlayerWW);
+    }
+
+    @Override
+    public void removeAffectedPlayer(IPlayerWW iPlayerWW) {
+        this.affectedPlayers.remove(iPlayerWW);
+    }
+
+    @Override
+    public void clearAffectedPlayer() {
+        this.affectedPlayers.clear();
+    }
+
+    @Override
+    public List<? extends IPlayerWW> getAffectedPlayers() {
+        return this.affectedPlayers;
+    }
+
+    @Override
+    public int getUse() {
+        return use;
+    }
+
+    @Override
+    public void setUse(int i) {
+        if (use == 3) return;
+        use = i;
+        if (use == 3) disableAbilities();
+    }
+}
