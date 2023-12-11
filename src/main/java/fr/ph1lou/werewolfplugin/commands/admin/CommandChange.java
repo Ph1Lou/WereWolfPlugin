2a0624dd6ed297a1f9a48592e84600c4884dd99c
philj <phil@ou.fr>
2023-12-11 18:40:00 +0100

    - clean up fix bugs

--- src/main/java/fr/ph1lou/werewolfplugin/commands/admin/CommandChange.java
+++ src/main/java/fr/ph1lou/werewolfplugin/commands/admin/CommandChange.java
@@ -7,8 +7,6 @@ import fr.ph1lou.werewolfapi.enums.StateGame;
 import fr.ph1lou.werewolfapi.game.WereWolfAPI;
 import org.bukkit.entity.Player;
 
-import java.io.IOException;
-
 @AdminCommand(key = "werewolf.commands.admin.change.command",
         descriptionKey = "werewolf.commands.admin.change.description",
         statesGame = StateGame.LOBBY,
@@ -18,13 +16,8 @@ public class CommandChange implements ICommand {
     @Override
     public void execute(WereWolfAPI game, Player player, String[] args) {
 
-
         player.sendMessage(game.translate(Prefix.YELLOW, "werewolf.commands.admin.change.in_progress"));
-        try {
-            game.getMapManager().loadMap();
-        } catch (IOException ignored) {
-        }
-
+        game.getMapManager().loadMap();
         player.sendMessage(game.translate(Prefix.GREEN, "werewolf.commands.admin.change.finished"));
 
     }
